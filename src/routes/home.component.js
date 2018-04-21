8d69ed5454bed1bd8898949d340df3a18f529638
zosia <zofia.subocz@gmail.com>
2018-04-21 09:20:20 +0200

    toggle background style

--- src/routes/home.component.js
+++ src/routes/home.component.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { Container } from "./home.style";
 
 class Home extends Component {
+    state = {
+        toggleBackground: true,
+    };
+
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+
+    toggleBackground = () => {
+        this.setState({
+            toggleBackground: !this.state.toggleBackground,
+        })
+    }
+
     render() {
         return (
-            <div>
+            <Container onClick={this.toggleBackground} active={this.state.toggleBackground}>
                 home
-            </div>
+            </Container>
         );
     }
 }
