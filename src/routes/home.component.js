01674eea9b676564335094e8c51c58bfb6a0ff14
zosia <zofia.subocz@gmail.com>
2018-04-21 12:19:57 +0200

    add user to state

--- src/routes/home.component.js
+++ src/routes/home.component.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
-import { Container } from "./home.style";
 
+//styles
+import { Container, UserLoginBox, UserInput, UserButton } from "./home.style";
+import { Wrapper } from "../utils/styles/global.style";
+
+//components
 import Header from "../components/header/header.component";
 import Footer from "../components/footer/footer.component";
 
 class Home extends Component {
+    state = {
+        users: [],
+        userValue: "",
+    };
+
+    componentDidMount() {
+        console.log('state component mount :', this.state)
+    };
+
+    componentDidUpdate() {
+        console.log('state component mount :', this.state)
+    };
+
+    addNewUser = () => {
+        this.setState({
+            users: [...this.state.users, this.state.userValue],
+            userValue: "",
+        })
+    };
+
+    userOnChange = (event) => {
+        this.setState({userValue: event.target.value});
+    };
+
+
 
     render() {
         return (
             <Container>
                 <Header />
-                home
+                <Wrapper>
+                    <UserLoginBox>
+                        <div>Please add GitHub user name to observable.</div>
+                        <UserInput value={this.state.userValue} onChange={this.userOnChange}/>
+                        <UserButton
+                            onClick={this.addNewUser}>
+                            Add user
+                        </UserButton>
+                    </UserLoginBox>
+                </Wrapper>
                 <Footer />
             </Container>
         );
--- src/routes/home.style.js
+++ src/routes/home.style.js
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
+import { colors } from '../utils/styles/color.style';
 
 export const Container = styled.div`
     background: 
  `;
+
+export const UserLoginBox = styled.div`
+    border: 1px solid ${colors.dark};
+    border-radius: 2.5px;
+    margin: 50px auto;
+    text-align: center;
+    padding: 20px;
+    width: 500px;
+`;
+
+export const UserInput = styled.input`
+    
+
+`;
+
+export const UserButton = styled.button`
+    
+
+`;
